38fa2d642002bf2a0bf54ae3d194bae9fe181fb5
kimyuuuuuu <helloaiworld01@gmail.com>
2024-05-29 18:56:04 +0900

    test 경로 수정

---
+++ chatbot3_last/model_intent_test.py
@@ -0,0 +1,16 @@
+from chatbot3_last.utils_list.Preprocess import Preprocess
+from chatbot3_last.models.intent.IntentModel import IntentModel
+
+p = Preprocess(word2index_dic='./chatbot3_last/train_tools/dict/chatbot_dict.bin',
+               userdic='./chatbot3_last/utils/user_dic.tsv')
+
+intent = IntentModel(model_name='./chatbot3_last/models/intent/intent_model.h5', preprocess=p)
+query = ["원주대 중앙동아리에는 뭐가 있어?", "운동 동아리 알려줘", "CCC 회비 얼마야", "프레이즈에 대해 알려줘", "취업동아리 어떻게 만들어", 'CCC에 대해 알려줘', '안녕']
+
+for i in query :
+  predict = intent.predict_class(i)
+  predict_label = intent.labels[predict]
+
+  print(i)
+  print("의도 예측 클래스 : ", predict)
+  print("의도 예측 레이블 : ", predict_label)
--- chatbot3_last/model_ner_test.py
+++ chatbot3_last/model_ner_test.py
@@ -1,11 +1,11 @@
-from utils.Preprocess import Preprocess
-from models.ner.NerModel import NerModel
+from chatbot3_last.utils_list.Preprocess import Preprocess
+from chatbot3_last.models.ner.NerModel import NerModel
 
-p = Preprocess(word2index_dic='C:/chatbot/chatbot1_ner/train_tools/dict/chatbot_dict.bin',
-               userdic='C:/chatbot/chatbot1_ner/utils/user_dic.tsv')
+p = Preprocess(word2index_dic='./chatbot3_last/train_tools/dict/chatbot_dict.bin',
+               userdic='./chatbot3_last/utils/user_dic.tsv')
 
 
-ner = NerModel(model_name='C:/chatbot/chatbot1_ner/models/ner/ner_model_testNER2.h5', proprocess=p)
+ner = NerModel(model_name='./chatbot3_last/models/ner/ner_model_testNER2.h5', proprocess=p)
 query = ['학습동아리에 대해 알려줘']
 
 for i in query :
